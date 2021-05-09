f8f99def12c1d4ed704600e7ba037c242e9b4487
AngelinaZhukovich <angelinka04@mail.ru>
2021-05-09 16:29:28 +0300

    cart.component

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CartService } from '../../services/cart/cart.service';
-
+import {  FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-cart',
@@ -8,13 +8,51 @@ import { CartService } from '../../services/cart/cart.service';
   styleUrls: ['./cart.component.scss']
 })
 export class CartComponent implements OnInit {
+  orderForm = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.minLength(2)]),
+    address: new FormControl(''),
+    paymentType: new FormControl('')
+  });
+
 
   items: any[] = [];
 
-  constructor(private cart: CartService) { }
+  constructor(private cart: CartService) {
+  }
 
   ngOnInit(): void {
     this.items = this.cart.getCartItems();
+    this.orderForm.valueChanges.subscribe((v) => {
+      console.log(this.orderForm.controls['name'])
+    })
+  }
+
+  onSubmit() {
+    let result = Object.assign(
+      this.orderForm.value, 
+      { items: this.cart.getCartItems() }
+    );
+    console.log(result);
+  }
+
+  addOne(id: number) {
+    this.items = this.items.map((el) => {
+      if (el.id === id) {
+        el.amount++;
+      }
+      return el;
+    });
+    this.cart.setItemsInCart(this.items);
+  }
+
+  removeOne(id: number){
+    this.items = this.items.map((el) => {
+      if (el.id === id) {
+        el.amount--;
+      }
+      return el;
+    });
+    this.cart.setItemsInCart(this.items);
   }
 
 }
--- src/app/services/cart/cart.service.ts
+++ src/app/services/cart/cart.service.ts
@@ -1,20 +1,47 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
-
+import { Product } from '../../interfaces/product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
 
-  private items: any[] = [];
-  private itemsInCart: number = 0;
-  private itemsInCart$ = new BehaviorSubject<number>(0);
+  private items: Array<Product> = JSON.parse(localStorage.getItem('items') as string) || [];
+  private itemsInCart: number = this.calculateItemsInCart(this.items);
+  private itemsInCart$ = new BehaviorSubject<number>(this.itemsInCart);
 
-  public setItemsInCart(item: Object) {
-    this.itemsInCart++;
-    this.items.push(item);
+  public addItemInCart(item: Product) {
+    if (this.items.length === 0) {
+      item.amount = 1;
+      this.items.push(item);
+    } else {
+      for (let i of this.items) {
+        if (i.id === item.id) {
+          i.amount ? i.amount++ : i.amount = 1;
+        }
+      }
+    }
+
+    let idsArray = this.items.map((item) => {
+      return item.id;
+    });
+    if (!idsArray.includes(item.id)) {
+      item.amount = 1;
+      this.items.push(item);
+    }
+
+    this.itemsInCart = this.calculateItemsInCart(this.items);
+    localStorage.setItem('items', JSON.stringify(this.items));
     this.itemsInCart$.next(this.itemsInCart);
+  }
+
+  public setItemsInCart(items: Array<Product>) {
+    this.items = [...items];
+    this.itemsInCart = this.calculateItemsInCart(this.items);
+    localStorage.setItem('items', JSON.stringify(this.items));
+    this.itemsInCart$.next(this.itemsInCart);
+
   }
 
   public getSubscription() {
@@ -23,6 +50,14 @@ export class CartService {
 
   public getCartItems(): any[] {
     return this.items;
+  }
+
+  private calculateItemsInCart(items: any[]): number {
+      let result = items.reduce((total, value) => {
+        return value.amount ? total + value.amount : total + 1;
+      }, 0);
+      console.log(result);
+      return result;
   }
 
   constructor() { }
